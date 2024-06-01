7b827d7769eee43b3a2f986ba60168dfd87f7b72
Frank Osorio <frankosorio4@gmail.com>
2024-05-31 22:22:57 -0300

    Adding 'criar'

--- src/classes/LivroCrud.js
+++ src/classes/LivroCrud.js
@@ -1,9 +1,46 @@
+const fs = require('fs')
+
 class LivroCrud {
 
     constructor() {
         this.filePath = './src/files/livros.json';
     }
 
+    criar(livro){
+        //archivo original contiene array
+        //leyendo y guardando lo que hay en una variable lo que hay el array
+        const conteudoActual = JSON.parse(fs.readFileSync(this.filePath,'utf8'));
+
+        //transformando a objeto y adjuntando la nueva data en el array
+        conteudoActual.push({
+            nome: livro.getNome,
+            autor: livro.getAutor,
+            codigo: livro.getCodigo,
+            generoLiterario: livro.getGeneroLiterario,
+            quantidadePaginas: livro.getQuantidadePaginas
+        });
+
+        //guardando en el archivo
+        fs.writeFileSync(
+            this.filePath,
+            JSON.stringify(conteudoActual, null, 2),
+            'utf8'
+        );
+    }
+
+    consultar(palavra){
+        const conteudoActual = JSON.parse(fs.futimesSync(this.filePath,'utf8'));
+
+        const livroEncontrado = conteudoActual.find( (livro) =>{
+            livro.autor === palavra
+        })
+
+        if(livroEncontrado){
+            console.log("livro encontrado")
+        } else{
+            console.log("livro nao encontrado")
+        }
+    }
 }
 
 module.exports = LivroCrud;
--- src/livros.js
+++ src/livros.js
@@ -1,4 +1,6 @@
 const readline = require('readline/promises');
+const Livro = require('./classes/Livro');
+const LivroCrud = require('./classes/LivroCrud');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,8 +13,31 @@ async function run() {
 
     switch (resposta) {
         case 'criar':
-            /* Coloque sua resposta aqui  */
+            
+            const nome = await rl.question('Qual e o nome do livro? ');
+            const livro = new Livro(nome)
+
+            const quantidadePaginas = await rl.question('Digite o numero de paginas do livro: ');
+            const generoLiterario = await rl.question('Qual e u genero Literario? ');
+            const autor = await rl.question('Digite o autor do livro: ');
+
+            livro.setAutor = autor;
+            livro.setQuantidadePaginas = quantidadePaginas;
+            livro.setGeneroLiterario = generoLiterario;
+            
             rl.close();
+
+            const livroCrud = new LivroCrud()
+            livroCrud.criar(livro);
+            
+            console.log("nome: ", livro.getNome);
+            console.log("autor: ", livro.getAutor);
+            console.log("genero: ", livro.getGeneroLiterario);
+            console.log("numero pag: ", livro.getQuantidadePaginas);
+            console.log("cod: ", livro.getCodigo);
+
+            // const livro = new Livro('', nome, quantidadePaginas, generoLiterario, autor)
+            // livro.setNome = nome;
             break;
         case 'deletar': {
             /* Coloque sua resposta aqui */
@@ -21,6 +46,12 @@ async function run() {
         }
         case 'consultar': {
             /* Coloque sua resposta aqui */
+            const  palavra = await rl.question("Autor do libro a buscar:")
+
+            const livroCrud = new LivroCrud();
+            livroCrud.consultar(palavra);
+
+
             rl.close();
             break;
         }
